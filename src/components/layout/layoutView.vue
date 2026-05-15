5cadf678443f4b190d45b8a3b759aedc461925f4
Hasan Yusuf Khan <hasanrafsun5@gmail.com>
2026-05-16 00:32:57 +0600

    project setup done

---
+++ src/components/layout/layoutView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Footer from './footer.vue';
+import Header from './header.vue';
+
+</script>
+
+<template>
+    <Header />
+    <main>
+        <router-view />
+    </main>
+    <Footer />
+</template>
